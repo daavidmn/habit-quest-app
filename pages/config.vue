9109abeab94d1dad8db4dbdfdb743581b21559ab
WendelT <wendelplayer@gmail.com>
2020-09-21 03:28:54 -0300

    Update config.vue
    Tela Configuração Completa, estilizada!

--- pages/config.vue
+++ pages/config.vue
@@ -1,28 +1,184 @@
 <template>
-  <view class="app">
-    <text>Pagina config</text>
-    <button title="ir para o inicio" :on-press="goToPage" :navigation="this.props.navigation" />
+  <view class="config">
+    <text class="titulo">Configurações</text>
+    <text class="subtitulo">Trocar de nome</text>
+
+    <text-input
+      class="input"
+      :style="{height: 40, padding:10, width: '100%', borderColor: '#6A994E', borderWidth: 2, borderRadius: 8}"
+    />
+
+    <touchable-opacity class="confirma-creditos">
+      <text class="text-confirma-creditos">Confirmar</text>
+    </touchable-opacity>
+
+    <text class="subtitulo">Notificações</text>
+    <view class="notificacoes">
+      <text>Permitir Notificações</text>
+      <switch
+        :on-value-change="handleChange1"
+        :value="value1"
+        :track-color="{ false: '#F0F0F0', true: '#A7C957' }"
+        thumb-color="#386641"
+      />
+    </view>
+
+    <view class="notificacoes">
+      <text>Permitir Roubo de Dados</text>
+      <switch
+        :on-value-change="handleChange2"
+        :value="value2"
+        :track-color="{ false: '#F0F0F0', true: '#A7C957' }"
+        thumb-color="#386641"
+      />
+    </view>
+
+    <text class="subtitulo2" :style="{marginTop: 40}">Créditos do Aplicativo</text>
+    <text>Veja quem ajudou a produzir esse aplicativo</text>
+    <touchable-opacity class="confirma-creditos">
+      <text class="text-confirma-creditos">Ver créditos</text>
+    </touchable-opacity>
+
+    <text class="subtitulo2" :style="{marginTop: 40}">Gerenciamento de conta</text>
+
+    <text>Cuidado, essas ação é irreversível</text>
+    <touchable-opacity class="resetar">
+      <text class="text-confirma-creditos">Resetar progresso</text>
+    </touchable-opacity>
   </view>
 </template>
 
 <script>
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
 export default {
+  data: function () {
+    return {
+      value1: false,
+      value2: false,
+    };
+  },
+  methods: {
+    handleChange1: function (val) {
+      this.value1 = val;
+    },
+    handleChange2: function (val) {
+      this.value2 = val;
+    },
+  },
   props: {
     navigation: {
       type: Object,
     },
   },
-  methods: {
-    goToPage() {
-      this.navigation.navigate("inicio");
-    },
-  },
 };
 </script>
 
-<style>
-.app {
+
+<style scoped>
+.config {
   width: 100%;
   height: 100%;
+  padding-right: 16px;
+  padding-left: 16px;
+  background-color: #e5e5e5;
+}
+
+.titulo {
+  height: 38px;
+  width: 189px;
+  left: 0px;
+  top: 0px;
+  margin-bottom: 24px;
+  border-radius: nullpx;
+  font-size: 28px;
+  font-style: normal;
+  font-weight: bold;
+  line-height: 38px;
+  text-align: left;
+}
+
+.input {
+  background-color: white;
+}
+
+.subtitulo {
+  height: 27px;
+  width: 100%;
+  left: 0px;
+  top: 0px;
+  margin-bottom: 8px;
+  border-radius: nullpx;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 27px;
+  text-align: left;
+}
+
+.subtitulo2 {
+  height: 27px;
+  width: 100%;
+  left: 0px;
+  top: 0px;
+  border-radius: nullpx;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 27px;
+  text-align: left;
+}
+
+.confirma-creditos {
+  height: 38px;
+  width: 100%;
+  left: 0px;
+  top: 4px;
+  margin-top: 8px;
+  margin-bottom: 24px;
+  padding-top: 9px;
+  border-radius: 8px;
+  background-color: #386641;
+}
+
+.resetar {
+  height: 38px;
+  width: 100%;
+  left: 0px;
+  top: 4px;
+  margin-top: 8px;
+  margin-bottom: 24px;
+  padding-top: 9px;
+  border-radius: 8px;
+  background-color: #bc4749;
+}
+
+.text-confirma-creditos {
+  height: 19px;
+  width: 100%;
+  left: 0px;
+  top: 0px;
+  border-radius: nullpx;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: bold;
+  line-height: 19px;
+  text-align: center;
+  color: white;
+  align-self: center;
+}
+
+.notificacoes {
+  width: 100%;
+  height: 40px;
+  left: 0px;
+  background-color: #ffffff;
+  border-radius: 8px;
+  flex-direction: row;
+  padding-left: 16px;
+  align-content: center;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 8px;
 }
 </style>

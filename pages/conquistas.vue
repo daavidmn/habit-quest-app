0308f9f40324c5a07e7b60f36f4023cc131af292
WendelT <wendelplayer@gmail.com>
2020-09-23 17:26:59 -0300

    Coleção
    Estilo da página coleção atualizado!

---
+++ pages/conquistas.vue
@@ -0,0 +1,28 @@
+<template>
+  <view class="app">
+    <text>Conquistas teste de navegação</text>
+    <button title="ir para expedicao" :on-press="goToPage" :navigation="this.props.navigation" />
+  </view>
+</template>
+
+<script>
+export default {
+  props: {
+    navigation: {
+      type: Object,
+    },
+  },
+  methods: {
+    goToPage() {
+      this.navigation.navigate("expedicao", "teste");
+    },
+  },
+};
+</script>
+
+<style>
+.app {
+  width: 100%;
+  height: 100%;
+}
+</style>
--- pages/inicio.vue
+++ pages/inicio.vue
@@ -91,7 +91,6 @@ export default {
 
   created() {
     this.$store.dispatch("fetchUsuario");
-
   },
 
   props: {

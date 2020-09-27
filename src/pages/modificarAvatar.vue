9eaa7921e9300743baf66b7e5467c71e263cc6c9
David Macedo <david@alu.ufc.br>
2020-09-27 18:30:40 -0300

    pagina de avatar criada

---
+++ src/pages/modificarAvatar.vue
@@ -0,0 +1,20 @@
+<template>
+  <view class="container"> </view>
+</template>
+
+<script>
+export default {
+  props: {
+    navigation: {
+      type: Object,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  width: 100%;
+  height: 100%;
+}
+</style>
